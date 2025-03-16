48182313a88bc064682e3c4e5a778c831a43ade5
igorstein3 <igorstein3@gmail.com>
2025-03-17 01:01:49 +0200

    designed

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,21 @@
 'use strict';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+const dateTimeInput = document.querySelector("#datetime-picker");
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+const fp = flatpickr(dateTimeInput, options);  // flatpickr
+// iziToast.show({
+//     title: 'Hey',
+//     message: 'What would you like to add?'
+// });
+
