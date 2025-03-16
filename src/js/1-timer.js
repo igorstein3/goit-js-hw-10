4f46e73a041a0db8d71d5a339a7767fd7d77df14
igorstein3 <igorstein3@gmail.com>
2025-03-17 01:41:49 +0200

    udate

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,6 +3,7 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import iconErr from "../img/alert-icon.svg";
 const dateTimeInput = document.querySelector("#datetime-picker");
 const options = {
   enableTime: true,
@@ -14,8 +15,58 @@ const options = {
   },
 };
 const fp = flatpickr(dateTimeInput, options);  // flatpickr
-// iziToast.show({
-//     title: 'Hey',
-//     message: 'What would you like to add?'
-// });
+iziToast.show({
+    id: null, 
+    class: '',
+    title: '',
+    titleColor: '',
+    titleSize: '',
+    titleLineHeight: '',
+    message: 'Please choose a date in the future',
+    messageColor: '#fff',
+    messageSize: '16px',
+    messageLineHeight: '150%',
+    backgroundColor: '#ef4040',
+    theme: 'light', // dark
+    color: '', // blue, red, green, yellow
+    icon: '../../../../src/img/alert-icon.svg',
+    iconText: '',
+    iconColor: '#fff',
+    iconUrl: iconErr,
+    image: '',
+    imageWidth: 50,
+    maxWidth: null,
+    zindex: null,
+    layout: 1,
+    balloon: false,
+    close: true,
+    closeOnEscape: false,
+    closeOnClick: false,
+    displayMode: 0, // once, replace
+    position: 'topRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
+    target: '',
+    targetFirst: true,
+    timeout: 5000,
+    rtl: false,
+    animateInside: true,
+    drag: true,
+    pauseOnHover: true,
+    resetOnHover: false,
+    progressBar: true,
+    progressBarColor: '',
+    progressBarEasing: 'linear',
+    overlay: false,
+    overlayClose: false,
+    overlayColor: 'rgba(0, 0, 0, 0.6)',
+    transitionIn: 'fadeInUp',
+    transitionOut: 'fadeOut',
+    transitionInMobile: 'fadeInUp',
+    transitionOutMobile: 'fadeOutDown',
+    buttons: {},
+    inputs: {},
+    onOpening: function () {},
+    onOpened: function () {},
+    onClosing: function () {},
+    onClosed: function () {}
+});
 
